c06b794a70c8c1cd66509b9bf5a82ea32a3bd293
Aabid <sayeedaabid0@gmail.com>
2022-03-29 17:40:07 +0530

    Real Estate UI Design

---
+++ android/app/src/main/kotlin/com/example/homedesign/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.homedesign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:homedesign/screens/landing_screen.dart';
+import 'dart:ui';
+
+import 'package:homedesign/utils/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = window.physicalSize.width;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HomeHouse',
+      theme: ThemeData(
+          primaryColor: COLOR_WHITE,
+          accentColor: COLOR_DARK_BLUE,
+          textTheme: screenWidth < 500 ? TEXT_THEME_SMALL : TEXT_THEME_DEFAULT,
+          fontFamily: 'Montserrat'),
+      home: const LandingScreen(),
+    );
+  }
+}
